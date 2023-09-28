74fa769f3caac538b86754e2f380513495728fed
Ruzeda <141177622+Ruzeda@users.noreply.github.com>
2023-09-28 15:25:54 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="component-container red">
+        <h1>component 1</h1>
+        {/* put Component1 RIGHT here */}
+      </div>
+      <div className="component-container blue">
+        <h1>component 2</h1>
+        {/* put component2 RIGHT here */}
+      </div>
+      <div className="component-container green">
+        <h1>component 3</h1>
+        {/* put component3 RIGHT here */}
+      </div>
+      <div className="component-container grey">
+        <h1>component 4</h1>
+        {/* put component4 RIGHT here */}
+      </div>
+      <div className="component-container purple">
+        <h1>component 5</h1>
+        {/* put component5 RIGHT here */}
+      </div>
+      <div className="component-container purple">
+        <h1>component 6</h1>
+        {/* put component5 RIGHT here */}
+      </div>
+    </div>
+  );
+}
